3a857fd9cada251def78678a3bbf1c1e686781a2
Mohammad Mahdi Niknam <141505892+Niknam1382@users.noreply.github.com>
2024-01-22 23:18:33 +0330

    first commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model) :
+    # image
+    # author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
